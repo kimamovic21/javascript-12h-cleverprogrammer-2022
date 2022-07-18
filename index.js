99aee5810c3ea80b4e7e4e2190c26c760fd0ede0
kimamovic21 <imamovic.kerim@gmail.com>
2022-07-18 17:26:04 +0200

    izmjene

--- index.js
+++ index.js
@@ -327,9 +327,96 @@ console.log(square([1, 2, 3, 4, 5, 6]));*/
 
 // 07 Array and object exercises
 
-//02:04:34 video
+/*const howManyLetters = () => {
+  const phrase = 'Hey, can you go to grocery store with me?'
+  //for of
+  for (const letter of phrase)  {
+    console.log(letter)
+  }
+}
+console.log(howManyLetters());*/
 
 
 
+/*const howManyLetters = () => {
+  const phrase = 'Hey, can you go to grocery store with me?'
+  //for in
+  for (const letter in phrase) {
+    console.log(Number(letter) + 1)
+  }
+}
+howManyLetters();*/
 
+
+/*const howManyLetters = (phrase) => {
+  
+  let result = 0;
+  for (const index in phrase) {
+    console.log(Number(index) + 1)
+    result = Number(index) + 1
+  }
+  return {result}
+}
+  const phrase = 'Hey, can you go to grocery store with me?'
+
+console.log(howManyLetters(phrase));*/
+
+/*const howManyLetters = (phrase) => {
+  let result = 0;
+  for (const index in phrase) {
+    console.log(Number(index) + 1)
+    result = Number(index) + 1
+  }
+  return {result}
+}
+  const phrase = prompt('Write your phrase');
+console.log(howManyLetters(phrase));*/
+
+
+/*const howManyLetters = (phrase) => {
+  return {result: phrase.length}
+}
+  const phrase = prompt('Write your phrase');
+console.log(howManyLetters(phrase));*/
+
+/*const sumArray = (numbers) => {
+  let result = 0; 
+  for (const number of numbers){
+   console.log(number);
+    result = result + number;
+  }
+  return {result};
+}
+const nums = [1,2,3,4,5]
+console.log(sumArray(nums));*/
+
+
+// phytontutor web page -> javascript
+
+
+/*const diffArray = (diffNumbers) => {
+  let diffResult = 0;
+  for (const number of diffNumbers) {
+    diffResult = diffResult - number;
+  }
+  return diffResult;
+}
+const diffNums = [20,10, 4, 3,2, 1];
+console.log(diffArray(diffNums));*/
+
+
+/*const max = (numbers) => {
+  let result = numbers[0]
+  // for loop
+  for (const number of numbers) {
+    if (number > result) {
+      result = number
+    }
+  }
+  return {result};
+}
+console.log(max([1,2,3,4,5]));*/
+
+
+// 2:31:50
 
